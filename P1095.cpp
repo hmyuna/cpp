9c2e62295a345ad029ed173589b61d32b8dcffde
hemingyuna <2913423825@qq.com>
2018-12-19 22:14:56 +0800

    del

--- P1095.cpp
+++ P1095.cpp
@@ -1,10 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int f[300001];//表示f
+int f[300001]; //表示f
 int m, s, t;
+int mo;
 int main()
 {
 	cin >> m >> s >> t;
-
+	mo = m;
+	for (int i = 1; i <= t; i++)
+	{
+		if (mo >= 10)
+			f[i] = f[i - 1] + 60, mo -= 10;
+		else
+			f[i] = f[i - 1], mo += 4;
+	}
+	for (int i = 1; i <= t; i++)
+	{
+		f[i] = max(f[i], f[i - 1] + 17);
+		if(f[i] >= s){
+			cout << "Yes" << endl
+				 << i << endl;
+			return 0;
+		}
+	}
+	cout << "No" << endl
+		 << f[t] << endl;
 	return 0;
 }
