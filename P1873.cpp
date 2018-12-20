755df456b8f7ac6567240ed687ba27b611d2affa
hemingyuna <2913423825@qq.com>
2018-12-20 20:33:18 +0800

    R14843248

--- P1873.cpp
+++ P1873.cpp
@@ -1,5 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int n, m, a[1000001], l = 0, r;
+bool check(int x){
+	int ans = 0;
+	for (int i = 1; i <= n; i++){
+		ans += max(a[i] - x, 0);
+	}
+	return ans >= m;
+}
+int main()
+{
+	cin >> n >> m;
+	for(int i = 1; i <= n; i++){
+		cin >> a[i];
+		r = max(r, a[i]);
+	}
+	while(l + 3 <= r){
+		int mid = (l + r) / 2;
+		if(check(mid) == 1)
+			l = mid;
+		else
+			r = mid;
+	}
+	for(int i = r; i >= l; i--){
+		if(check(i) == 1){
+			cout << i << endl;
+			return 0;
+		}
+	}
+	cout << -1 << endl;
 	return 0;
 }
