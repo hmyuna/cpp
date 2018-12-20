42e107661238ec2785de2f9dd1f2754e5fcf7bba
hemingyuna <2913423825@qq.com>
2018-12-20 20:36:43 +0800

    R14843413 int->long long

--- P1873.cpp
+++ P1873.cpp
@@ -1,28 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a[1000001], l = 0, r;
-bool check(int x){
-	int ans = 0;
-	for (int i = 1; i <= n; i++){
-		ans += max(a[i] - x, 0);
+long long n;
+long long a[1000001];
+long long m, l = 0, r;
+bool check(long long x){
+	long long ans = 0;
+	for (long long i = 1; i <= n; i++){
+		ans += max(a[i] - x, (long long)0);
 	}
 	return ans >= m;
 }
 int main()
 {
 	cin >> n >> m;
-	for(int i = 1; i <= n; i++){
+	for(long long i = 1; i <= n; i++){
 		cin >> a[i];
 		r = max(r, a[i]);
 	}
 	while(l + 3 <= r){
-		int mid = (l + r) / 2;
+		long long mid = (l + r) / 2;
 		if(check(mid) == 1)
 			l = mid;
 		else
 			r = mid;
 	}
-	for(int i = r; i >= l; i--){
+	for(long long i = r; i >= l; i--){
 		if(check(i) == 1){
 			cout << i << endl;
 			return 0;
